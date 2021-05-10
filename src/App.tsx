66108c5e455bfe143c90c1d3f47d4501b49334cb
yiinuma <y.iinuma211@gmail.com>
2021-05-10 21:15:59 +0900

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import "./App.css";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
-    return (
-        <ChakraProvider>
-            <Button>ボタン</Button>
-        </ChakraProvider>
-    );
+	return (
+		<ChakraProvider theme={theme}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</ChakraProvider>
+	);
 }
