ab362136835a23604873ea8905dfad1759bce16e
yiinuma <y.iinuma211@gmail.com>
2021-05-10 18:43:06 +0900

    chakura

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
-import { Button,ChakraProvider } from '@chakra-ui/react';
+import "./App.css";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 export default function App() {
-  return (
-    <ChakraProvider>
-      <Button>ボタン</Button>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <Button>ボタン</Button>
+        </ChakraProvider>
+    );
 }
