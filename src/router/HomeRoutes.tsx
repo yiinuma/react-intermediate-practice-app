f5788202fe7b7dcdc9f4f570b1e6cc7fb6bfe5c4
yiinuma <y.iinuma211@gmail.com>
2021-05-11 23:31:25 +0900

    Header

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -4,24 +4,24 @@ import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
 
 export const homeRoutes = [
-  {
-    path: "/",
-    exact: true,
-    children: <Home />,
-  },
-  {
-    path: "/user_management",
-    exact: false,
-    children: <UserManagement />,
-  },
-  {
-    path: "/setting",
-    exact: false,
-    children: <Setting />,
-  },
-  {
-    path: "*",
-    exact: false,
-    children: <Page404 />,
-  },
+    {
+        path: "/",
+        exact: true,
+        children: <Home />,
+    },
+    {
+        path: "/user_management",
+        exact: false,
+        children: <UserManagement />,
+    },
+    {
+        path: "/setting",
+        exact: false,
+        children: <Setting />,
+    },
+    {
+        path: "*",
+        exact: false,
+        children: <Page404 />,
+    },
 ];
