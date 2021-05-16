f906e822c574fe38ccfafff8b320f05924295f5f
yiinuma <y.iinuma211@gmail.com>
2021-05-16 16:33:37 +0900

    userCard

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/layout";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="Hero"
+          fullName="Heroheroyoshi"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
